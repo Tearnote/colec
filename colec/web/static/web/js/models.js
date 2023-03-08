1290f3d04d44ae88b82440d3535ad0f78f2a5959
Tearnote <marach5@gmail.com>
2023-03-08 14:33:52 +0000

    Add example retrieval of collections via API

--- colec/web/static/web/js/models.js
+++ colec/web/static/web/js/models.js
@@ -10,7 +10,34 @@ const CollectionCollection = Backbone.Collection.extend({
     model: CollectionModel,
 });
 
-let collections = new CollectionCollection();
-collections.fetch().then(function() {
-  console.log(collections.length);
+const CollectionListItemView = Backbone.View.extend({
+    tagName: "li",
+    template: _.template(`
+        <p>Name: <%= name %></p>
+        <p>Description: <%= description %></p>
+        <p>Tags enabled: <%= tags_enabled %></p>
+    `),
+    render() {
+        const html = this.template(this.model.toJSON());
+        this.$el.html(html);
+        return this;
+    },
 });
+const CollectionListView = Backbone.View.extend({
+    el: "#collection-list",
+    initialize() {
+        this.listenTo(this.collection, "sync", this.render);
+    },
+    render() {
+        this.$el.empty();
+
+        this.collection.each(function(model) {
+            const item = new CollectionListItemView({model: model});
+            this.$el.append(item.render().$el);
+        }, this);
+        return this;
+    },
+});
+const collectionList = new CollectionCollection();
+const collectionView = new CollectionListView({collection: collectionList});
+collectionList.fetch();
