fc008e98b53ea01eea4e97cbfaf4a73e59145b3f
Tearnote <marach5@gmail.com>
2023-03-08 10:05:35 +0000

    Add a router to switch to sign-in URL

--- colec/web/static/web/js/app.js
+++ colec/web/static/web/js/app.js
@@ -1,9 +1,19 @@
 const signInModalTemplateText = $("#sign-in-modal-template").html();
 const signInModalTemplate = _.template(signInModalTemplateText);
 
-document.getElementById("sign-in-button").addEventListener("click", () => {
-    const html = signInModalTemplate();
-    let el = $.parseHTML(html);
-    console.log(el);
-    $("body").append(el);
+let App = Backbone.Router.extend({
+    routes: {
+        "signin": "signin",
+    },
+    signin: function() {
+        const html = signInModalTemplate();
+        let el = $.parseHTML(html);
+        $("body").append(el);
+    },
+});
+let app = new App();
+Backbone.history.start({pushState: true});
+
+$("#sign-in-button").on("click", function() {
+    app.navigate("signin", {trigger: true});
 });
