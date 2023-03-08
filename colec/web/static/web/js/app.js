be05c8ff244e7f6f23d04b8b0001e057fdb9d9cb
Tearnote <marach5@gmail.com>
2023-03-08 11:51:01 +0000

    Factor out functions to show and hide modals

--- colec/web/static/web/js/app.js
+++ colec/web/static/web/js/app.js
@@ -8,17 +8,30 @@ $(document).on("click", "a", function(e) {
 const App = Backbone.Router.extend({
     contentEl: document.getElementById("content"),
     modalEl: document.getElementById("modal"),
+    modalShown: false,
     routes: {
         "": "index",
         "signin": "signin",
     },
     index() {
+        this.hideModal();
         this.contentEl.innerHTML = Templates.landingPage();
     },
     signin() {
         this.index();
-        this.modalEl.innerHTML = Templates.signInModal();
+        this.showModal(Templates.signInModal());
+    },
+    showModal(html) {
+        if (this.modalShown) this.hideModal();
+        this.modalEl.innerHTML = html;
         document.documentElement.classList.add("modal-is-open");
+        this.modalShown = true;
+    },
+    hideModal() {
+        if (!this.modalShown) return;
+        this.modalEl.replaceChildren();
+        document.documentElement.classList.remove("modal-is-open");
+        this.modalShown = false;
     },
 });
 
