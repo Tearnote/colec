3bf2ccc885c230aed8ce7b5901e19a73c9938ac3
Tearnote <marach5@gmail.com>
2023-03-08 14:51:49 +0000

    Create views for user auth

--- colec/auth_api/urls.py
+++ colec/auth_api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path, include
 
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('me/', views.me),
+]
--- colec/auth_api/views.py
+++ colec/auth_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+@api_view(['POST'])
+def login(request):
+    login(request, request.user)
+    return Response(UserSerializer(request.user).data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    logout(request)
+    return Response({})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    return Response(UserSerializer(request.user).data)
